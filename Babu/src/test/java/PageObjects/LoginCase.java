af50c94a70ddefb2a6739f1e3ff35cd8a9d5faf7
Lenovo <Lenovo@DESKTOP-VDRSD1O>
2022-01-13 23:09:28 +0530

    POM concept added

---
+++ Babu/src/test/java/PageObjects/LoginCase.java
@@ -0,0 +1,36 @@
+package PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class LoginCase {
+
+
+	
+	@FindBy(name="q")
+	public static WebElement userName;
+	
+	@FindBy(className ="gNO89b")
+	public static WebElement password;
+	
+	@FindBy(className="btn btn-default btn-lg btn-block effect ladda-button waves-effect")
+	public static WebElement Singin;
+	
+	
+	/*
+	 * public static WebElement username(WebDriver driver) {
+	 * 
+	 * return driver.findElement(By.id("id")); } public static WebElement
+	 * password(WebDriver driver) { return driver.findElement(By.id("id"));
+	 * 
+	 * }
+	 * 
+	 * public static WebElement loginButton(WebDriver driver) { return
+	 * driver.findElement(By.id("id"));
+	 * 
+	 * }
+	 */
+}
---
+++ Babu/src/test/java/TestCases/LoginTC.java
@@ -0,0 +1,36 @@
+package TestCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+
+import PageObjects.LoginCase;
+
+public class LoginTC {
+
+	WebDriver driver;
+	@Test
+	public void logintest() {
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Lenovo\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.google.in");
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		PageFactory.initElements(driver,LoginCase.class);
+	
+		LoginCase.userName.sendKeys("user@phptravels.com");
+		LoginCase.password.click();
+		
+	/*
+	 * Login.username(driver).sendKeys("demo");
+	 * Login.password(driver).sendKeys("Password");
+	 * Login.loginButton(driver).click();
+	 */
+		
+	}
+}
